0e28222ea3637c7208d80b810ce2db4d999f5eb4
Albin Sandefalk <albinsand@hotmail.se>
2025-03-07 14:17:21 +0100

    Moved description to README.md

--- sysinfo.py
+++ sysinfo.py
@@ -1,14 +1,5 @@
 import os, time, datetime
 
-"""
-This is a script that collects and writes system information to a file.
-I use it to monitor the resource use on my Dell Poweredge R710.
-
-!- NOTE -!
-I have not yet tested it on anything other than my laptop so at the time of writing
-I am not sure if it will work on other systems.
-"""
-
 # Global variables
 log_file = ".log.txt"
 info_file = ".info.txt"
